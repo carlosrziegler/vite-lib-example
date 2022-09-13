e9914c5272a635563f80372887f0b34f1c190355
carlosrziegler <carlos.ziegler@prea.eu>
2022-09-13 12:31:54 +0200

    update vite

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,16 @@
 module.exports = {
-    framework: '@storybook/react',
-    stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-a11y', '@storybook/addon-links', '@storybook/addon-essentials'],
-    core: {
-        builder: 'storybook-builder-vite',
-    },
-    features: {
-        storyStoreV7: true,
-    },
-    async viteFinal(config) {
-        return config;
-    },
+  framework: '@storybook/react',
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-a11y', '@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: '@storybook/builder-vite'
+  },
+  features: {
+    storyStoreV7: true
+  },
+
+  async viteFinal(config) {
+    return config;
+  }
+
 };
